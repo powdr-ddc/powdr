52269d6cb12ec5abdf4fdc38aea84d7a06017007
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-12-07 14:09:08 -0700

    Updated User Fragment

--- app/src/main/java/edu/cnm/deepdive/powdr/service/UserRepository.java
+++ app/src/main/java/edu/cnm/deepdive/powdr/service/UserRepository.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import edu.cnm.deepdive.powdr.model.dto.User;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
+import java.util.List;
 
 public class UserRepository {
 
@@ -18,10 +19,12 @@ public class UserRepository {
     signInService = GoogleSignInService.getInstance();
   }
 
-  public Single<User> getProfileFromServer() {
+  public Single<User> getProfile() {
     return signInService.refreshBearerToken()
         .observeOn(Schedulers.io())
         .flatMap(webService::getProfile);
   }
+
+//  public Single<List<User>> getFriends()
 
 }
